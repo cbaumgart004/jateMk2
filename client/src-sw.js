8a683b412bcb6f8edb405f4eb304ebf9045608a3
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-16 19:47:22 -0600

    updated src-sw and webpack config based on mk1

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,12 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
+import { registerRoute } from 'workbox-routing'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
+import { ExpirationPlugin } from 'workbox-expiration'
+import { precacheAndRoute } from 'workbox-precaching'
+import { warmStrategyCache } from 'workbox-recipes'
 
-precacheAndRoute(self.__WB_MANIFEST);
+// Precache and route assets
+precacheAndRoute(self.__WB_MANIFEST)
 
+// Cache page navigations (HTML) with CacheFirst strategy
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,17 +16,47 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
     }),
   ],
-});
+})
 
+// Warm up the cache for critical pages like the homepage and index.html
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
-});
+})
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Cache pages (HTML) using the pageCache strategy
+registerRoute(({ request }) => request.mode === 'navigate', pageCache)
 
-// TODO: Implement asset caching
-registerRoute();
+// Cache static assets like JS and CSS with StaleWhileRevalidate strategy
+registerRoute(
+  ({ request }) =>
+    request.destination === 'script' || request.destination === 'style',
+  new StaleWhileRevalidate({
+    cacheName: 'static-assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+)
+
+// Cache images with CacheFirst strategy
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 50, // Only cache 50 images
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache images for 30 days
+      }),
+    ],
+  })
+)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,67 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
+const { InjectManifest } = require('workbox-webpack-plugin')
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'development', // Switch to 'production' for production builds
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './src/js/index.js', // Your entry points for JavaScript
+      install: './src/js/install.js',
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js', // Bundle naming pattern
+      path: path.resolve(__dirname, 'dist'), // Output directory
     },
     plugins: [
-      
+      // HtmlWebpackPlugin to copy index.html to the dist folder
+      new HtmlWebpackPlugin({
+        template: './client/index.html', // Path to index.html in client folder
+        title: 'PWA Text Editor',
+      }),
+      // WebpackPwaManifest to generate the manifest.json file
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'TextEditor',
+        description: 'A simple text editor Progressive Web Application',
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        start_url: './', // Starting URL for the PWA
+        publicPath: './', // Public path for assets
+        icons: [
+          {
+            src: path.resolve(__dirname, 'client/src/images/logo.png'), // Path to your logo
+            sizes: [96, 128, 192, 256, 384, 512], // Multiple icon sizes for PWA
+            destination: path.join('assets', 'icons'), // Destination folder inside dist
+          },
+        ],
+      }),
+      // InjectManifest to inject your custom service worker
+      new InjectManifest({
+        swSrc: './client/src-sw.js', // Custom service worker in client folder
+        swDest: 'service-worker.js', // Output file in dist
+      }),
     ],
-
     module: {
       rules: [
-        
+        // CSS loader to handle CSS files
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], // Loaders for CSS files
+        },
+        // Babel loader to transpile modern JavaScript
+        {
+          test: /\.m?js$/, // Matches both .js and .mjs files
+          exclude: /node_modules/, // Don't transpile node_modules
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'], // Preset for ES6+ support
+            },
+          },
+        },
       ],
     },
-  };
-};
+  }
+}
